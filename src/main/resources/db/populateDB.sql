199f2ae520e4c07425014495df405b634a4a361c
msamichev <msamichev@gmail.com>
2015-06-25 22:23:29 +0300

    HW3

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM user_meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -11,3 +12,11 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 
 INSERT INTO user_roles (role, user_id) VALUES ('ROLE_USER', 100000);
 INSERT INTO user_roles (role, user_id) VALUES ('ROLE_ADMIN', 100001);
+
+INSERT INTO user_meals (description, calories, user_id) VALUES ('description1', 555, 100000);
+INSERT INTO user_meals (description, calories, user_id) VALUES ('description2', 1000, 100000);
+INSERT INTO user_meals (description, calories, user_id) VALUES ('description3', 200, 100000);
+
+INSERT INTO user_meals (description, calories, user_id) VALUES ('description1', 555, 100001);
+INSERT INTO user_meals (description, calories, user_id) VALUES ('description2', 1000, 100001);
+INSERT INTO user_meals (description, calories, user_id) VALUES ('description3', 200, 100001);
