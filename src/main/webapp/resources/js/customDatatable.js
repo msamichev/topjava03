d44848a4cac58676095356c3252f3a271bacaa83
msamichev <msamichev@gmail.com>
2015-07-30 16:04:53 +0300

    Changes after lesson8(повторная, первая попытка не далась)

--- src/main/webapp/resources/js/customDatatable.js
+++ src/main/webapp/resources/js/customDatatable.js
@@ -1,11 +1,52 @@
+function makeEditable() {
+    $('#add').click(function () {
+        $('#item_id').val(0);
+        $('#editRow').modal();
+    });
+
+    $('.delete').click(function () {
+        deleteRow($(this).attr("id"));
+    });
+
+    $('#detailsForm').submit(function () {
+        save();
         return false;
     });
 
     $(document).ajaxError(function (event, jqXHR, options, jsExc) {
         failNoty(event, jqXHR, options, jsExc);
+    });
+}
+
+function deleteRow(id) {
+    $.ajax({
+        url: ajaxUrl + id,
+        type: 'DELETE',
         success: function () {
             updateTable();
             successNoty('Deleted');
+        }
+    });
+}
+
+function updateTable() {
+    $.get(ajaxUrl, function (data) {
+        oTable_datatable.fnClearTable();
+        $.each(data, function (key, item) {
+            oTable_datatable.fnAddData(item);
+        });
+        oTable_datatable.fnDraw();
+    });
+}
+
+function save() {
+    var frm = $('#detailsForm');
+    debugger;
+    $.ajax({
+        type: "POST",
+        url: ajaxUrl,
+        data: frm.serialize(),
+        success: function (data) {
             $('#editRow').modal('hide');
             updateTable();
             successNoty('Saved');
@@ -38,4 +79,5 @@ function failNoty(event, jqXHR, options, jsExc) {
         text: 'Failed: ' + jqXHR.statusText + "<br>",
         type: 'error',
         layout: 'bottomRight'
+    });
 }
